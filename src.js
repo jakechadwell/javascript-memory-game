8951c51d4b933dcdbaa1beae5a50217a67fa8e57
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-13 01:25:44 -0500

    styled boxes and navbar

--- src.js
+++ src.js
@@ -1,6 +1,11 @@
 "use strict";
 (function () {
-
-
-
+    $(document).ready(function (){
+        $(".box").mouseenter(function (){
+            $(this).css("background-color", "lightskyblue");
+        })
+        $(".box").mouseleave(function (){
+            $(this).css("background-color", "cornflowerblue");
+        })
+    })
 })();
