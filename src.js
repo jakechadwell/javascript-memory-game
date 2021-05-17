e369af7f33c1eb584a8120dc06d030647df50246
jake chadwell <jake.chadwell01@gmail.com>
2021-05-16 19:27:41 -0500

    simon squares function

--- src.js
+++ src.js
@@ -6,6 +6,29 @@
         }, function(){
             $(this).css("background-color", "#6495ed");
         })
+
+        let counter = 1;
+
+        function simonSquares(){
+            for(let i = 1; i<=counter; i++){
+                let bigDelay = 2000;
+                let bigTimeoutId = setTimeout(function(){
+
+                }, bigDelay)
+                    let simonChoice = Math.floor(Math.random() * 9);
+                    console.log(simonChoice+1);
+                    let selectedSquare = document.getElementById(simonChoice+1);
+                    $(selectedSquare).toggleClass("simon-square");
+                    let delay = 2000;
+                    let timeoutId = setTimeout(function(){
+                        $(selectedSquare).toggleClass("simon-square");
+                    }, delay);
+            }
+            counter++;
+        }
+
+        console.log(simonSquares());
+        console.log(simonSquares());
     })
 
     function getIdClicked(){
