5e5922a6eb82536d619b28f82afce28bcd577055
Jake Chadwell <jake.chadwell01@gmail.com>
2021-05-17 00:08:22 -0500

    added start button functionality

--- src.js
+++ src.js
@@ -1,41 +1,50 @@
 "use strict";
 (function () {
+    let counter = 1;
+
     $(document).ready(function (){
         $(".box").hover(function (){
             $(this).css("background-color", "lightskyblue");
         }, function(){
             $(this).css("background-color", "#6495ed");
+        });
+
+        $(".start-button").click(function(){
+            for(let i = 1; i<=counter; i++){
+                simonSquares();
+            }
+        });
+
+        $(".box").click(function (){
+
         })
 
-        let counter = 1;
+    });
 
-        function simonSquares(){
-            for(let i = 1; i<=counter; i++){
-                let bigDelay = 2000;
-                let bigTimeoutId = setTimeout(function(){
-
-                }, bigDelay)
-                    let simonChoice = Math.floor(Math.random() * 9);
-                    console.log(simonChoice+1);
-                    let selectedSquare = document.getElementById(simonChoice+1);
-                    $(selectedSquare).toggleClass("simon-square");
-                    let delay = 2000;
-                    let timeoutId = setTimeout(function(){
-                        $(selectedSquare).toggleClass("simon-square");
-                    }, delay);
-            }
-            counter++;
-        }
-
-        console.log(simonSquares());
-        console.log(simonSquares());
-    })
+    function simonSquares(){
+        let simonChoice = Math.floor(Math.random() * 9);
+        console.log(simonChoice+1);
+        let selectedSquare = document.getElementById(simonChoice+1);
+        $(selectedSquare).toggleClass("simon-square");
+        let delay = 2000;
+        let timeoutId = setTimeout(function(){
+            $(selectedSquare).toggleClass("un-simon-square");
+        }, delay);
+    }
 
     function getIdClicked(){
         $(".box").click(function(){
             let id = this.id;
-
         })
+    }
+
+    function match(simonChoice){
+        let userInput = getIdClicked();
+        if(userInput==simonChoice+1){
+            return userInput;
+        }else{
+            alert("Sorry, that is incorrect. You lasted: " + counter + " rounds!")
+        }
     }
 
 })();
