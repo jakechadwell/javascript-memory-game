bda6a9ffd7637a40aff808d709a3da7acb641b36
jake chadwell <jake.chadwell01@gmail.com>
2021-06-16 23:40:14 -0500

    added base functionality

--- src.js
+++ src.js
@@ -1,6 +1,9 @@
 "use strict";
 (function () {
-    let counter = 2;
+    let roundNumber = 1;
+    let arrNums = [];
+    let currentIndex = 0;
+    let gameOver = false;
 
     $(document).ready(function (){
         $(".box").hover(function (){
@@ -9,34 +12,54 @@
             $(this).css("background-color", "#6495ed");
         });
 
-        $(".start-button").click(function(){
-            for(let i = 1; i<=counter; i++){
-                simonSquares();
-            }
+        $(".start-button").click(function(){      
+            addNumber();
+            console.log("hello");
+						simonSquares(arrNums);
         });
 
+				if(gameOver==true){
+					alert("Game over! If you would like to play again, refresh the page!");
+				}
     });
 
-    function simonSquares(){
-        let simonChoice = Math.floor(Math.random() * 9);
-        console.log(simonChoice+1);
-        let selectedSquare = document.getElementById(simonChoice+1);
-        $(selectedSquare).toggleClass("simon-square");
-        let delay = 1000;
-        let timeoutId = setTimeout(function(){
-            $(selectedSquare).toggleClass("un-simon-square");
-        }, delay);
+    function addNumber(amount=1){
+      for (let i = 0; i < amount; i++){
+        arrNums.push(generateNum());
+      }
     }
 
-    function getIdClicked(){
-        $(".box").click(function(){
-            let id = this.id;
-        });
+    function generateNum(){
+      return Math.floor(Math.random() * 9);
     }
 
-    function match(simonChoice){
-        let userInput = getIdClicked();
-        return userInput===(simonChoice+1);
+    function simonSquares(arrNums){
+        console.log("hello");
+        for (let i = 0; i < arrNums.length; i++){
+          let selectedSquare = $("" + arrNums[i]);
+          $(selectedSquare).toggleClass("simon-square");
+          let timeoutId = setTimeout(function(){
+              $(selectedSquare).toggleClass("un-simon-square");
+          }, 1000);
+        }
+        $(".box").click(checkUserGuess($(this).attr("id"), arrNums[currentIndex]));
+    }
+
+    function checkUserGuess(guessId, correctId){
+      if (guessId == correctId) {
+        if (arrNums[currentIndex + 1] == null){
+          nextRound();
+        }
+        else currentIndex++;
+      }
+      else gameOver == true;
+
+    }
+
+    function nextRound() {
+      addNumber();
+			currentIndex=0;
+			simonSquares(arrNums);
     }
 
 })();
