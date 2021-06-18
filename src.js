faec84abe835f45dbe1b852edd61538cd6679713
Jake Chadwell <jake.chadwell01@gmail.com>
2021-06-18 17:14:31 -0500

    completion

--- src.js
+++ src.js
@@ -3,63 +3,92 @@
     let roundNumber = 1;
     let arrNums = [];
     let currentIndex = 0;
-    let gameOver = false;
 
     $(document).ready(function (){
-        $(".box").hover(function (){
-            $(this).css("background-color", "lightskyblue");
-        }, function(){
-            $(this).css("background-color", "#6495ed");
-        });
 
-        $(".start-button").click(function(){      
+        $(".start-button").click(function(){
             addNumber();
-            console.log("hello");
-						simonSquares(arrNums);
+            simonSquares(arrNums);
+            $(".start-button").off();
+            $(".start-button").css("display", "none");
+            $(".start-button").html("Next Round");
         });
 
-				if(gameOver==true){
-					alert("Game over! If you would like to play again, refresh the page!");
-				}
+        $("#current-round").text("Current Round: " + roundNumber);
     });
 
     function addNumber(amount=1){
-      for (let i = 0; i < amount; i++){
-        arrNums.push(generateNum());
-      }
+        for (let i = 0; i < amount; i++){
+            let newNum = generateNum();
+            while(newNum==arrNums[arrNums.length-1]){
+                newNum = generateNum();
+            }
+            arrNums.push(newNum);
+        }
     }
 
     function generateNum(){
-      return Math.floor(Math.random() * 9);
+        return Math.floor(Math.random() * 9 + 1);
     }
 
-    function simonSquares(arrNums){
-        console.log("hello");
+    async function simonSquare(selectedSquare) {
+        return new Promise(function(resolve, reject) {
+            $(selectedSquare).addClass("simon-square");
+            setTimeout(resolve, 1000);
+        }).then(function() {
+            $(selectedSquare).removeClass("simon-square");
+        });
+    }
+
+    async function simonSquares(arrNums){
         for (let i = 0; i < arrNums.length; i++){
-          let selectedSquare = $("" + arrNums[i]);
-          $(selectedSquare).toggleClass("simon-square");
-          let timeoutId = setTimeout(function(){
-              $(selectedSquare).toggleClass("un-simon-square");
-          }, 1000);
+            await simonSquare($(`#${arrNums[i]}`))
         }
-        $(".box").click(checkUserGuess($(this).attr("id"), arrNums[currentIndex]));
+        $(".box").click(checkUserGuess);
     }
 
-    function checkUserGuess(guessId, correctId){
-      if (guessId == correctId) {
-        if (arrNums[currentIndex + 1] == null){
-          nextRound();
+    function checkUserGuess(){
+        let guessId = $(this).attr("id");
+        let correctId = arrNums[currentIndex];
+        if (guessId == correctId) {
+            if (arrNums[currentIndex + 1] == null){
+                roundOver();
+            }
+            else currentIndex++;
         }
-        else currentIndex++;
-      }
-      else gameOver == true;
+        else gameOver();
 
+    }
+
+    function gameOver(){
+        $(".body").html(
+            `
+						<div>
+						<h1 class="text-center game-over-header">Game Over, you made it to round: ${roundNumber}<h1>
+            <a href="#" id="replay-button" class="w-25">Play again</a>
+						</div>
+        `
+        );
+        $("#replay-button").click(function(){
+            location.reload();
+        })
+    }
+
+    function roundOver() {
+        $(".start-button").css("display", "unset");
+        $(".start-button").html("Next Round");
+        $(".start-button").click(nextRound);
     }
 
     function nextRound() {
-      addNumber();
-			currentIndex=0;
-			simonSquares(arrNums);
+        addNumber();
+        currentIndex = 0;
+        simonSquares(arrNums);
+        $(".box").off();
+        $(".start-button").css("display", "none");
+        $(".start-button").off();
+        roundNumber++;
+        $("#current-round").text("Current Round: " + roundNumber);
     }
 
 })();
